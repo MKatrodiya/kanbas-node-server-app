e453899f718597dedb38277fd458cfb44e1f6e87
Meet <katrodiya.m@northeastern.edu>
2024-04-04 15:43:31 -0400

    MAde changes for modules and assignments

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import Lab5 from "./Lab5.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import cors from "cors";
+import AssignmentRoutes from "./Kanbas/assignments/routes.js";
 
 const app = express();
 app.use(cors());
@@ -11,6 +12,7 @@ app.use(express.json());
 
 CourseRoutes(app);
 ModuleRoutes(app);
+AssignmentRoutes(app);
 
 Hello(app);
 Lab5(app);
