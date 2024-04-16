e58d6765491a70f7ebaa684e8bebaad1de7e4ca7
Meet <katrodiya.m@northeastern.edu>
2024-04-16 10:34:11 -0400

    Started with a6

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
 import express from "express";
+import mongoose from "mongoose";
+import UserRoutes from "./Kanbas/Users/routes.js";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
@@ -6,6 +8,7 @@ import ModuleRoutes from "./Kanbas/modules/routes.js";
 import cors from "cors";
 import AssignmentRoutes from "./Kanbas/assignments/routes.js";
 
+mongoose.connect("mongodb://localhost:27017/kanbas");
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -13,6 +16,7 @@ app.use(express.json());
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
+UserRoutes(app);
 
 Hello(app);
 Lab5(app);
